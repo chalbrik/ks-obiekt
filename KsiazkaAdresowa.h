5704f4f517ff35bbb663cb2d83973a16fcc38b4a
chalbrik <paw.swa95@gmail.com>
2023-08-22 12:14:47 +0200

    Add registered user to file

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <windows.h>
+#include <sstream>
+#include <fstream>
+
 #include "Uzytkownik.h"
 
 using namespace std;
@@ -10,9 +14,24 @@ class KsiazkaAdresowa
     int idOstatniegoAdresata;
     int idUsunietegoAdresata;
 
+    string nazwaPlikuZUzytkownikami;
+
     vector <Uzytkownik> uzytkownicy;
 
+    string wczytajLinie();
+
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    string konwerjsaIntNaString(int liczba);
+    bool czyPlikJestPusty(fstream &plikTekstowy);
+
+
 public:
+    KsiazkaAdresowa();
     void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
 
 };
--- Uzytkownik.h
+++ Uzytkownik.h
@@ -2,9 +2,18 @@
 
 using namespace std;
 
-class Uzytkownik
-{
+class Uzytkownik {
+
     int id;
     string login;
     string haslo;
+
+public:
+    void ustawId(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string noweHaslo);
+
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();
 };
