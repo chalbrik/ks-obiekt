5d5973c2bf8e40ae2b1c935a3ff36f8312cdb8c9
chalbrik <paw.swa95@gmail.com>
2023-08-25 13:58:20 +0200

    Divide KsiaskaAdresowa to smaller class

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,37 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <windows.h>
-#include <sstream>
-#include <fstream>
+#ifndef KSIAZKAADRESOWA_H
+#define KSIAZKAADRESOWA_H
 
-#include "Uzytkownik.h"
+#include <iostream>
+
+#include "UzytkownikMenedzer.h"
 
 using namespace std;
 
 class KsiazkaAdresowa
 {
-    int idZalogowanegoUzytkownika;
-    int idOstatniegoAdresata;
-    int idUsunietegoAdresata;
-
-    string nazwaPlikuZUzytkownikami;
-
-    vector <Uzytkownik> uzytkownicy;
-
-    string wczytajLinie();
-
-    Uzytkownik podajDaneNowegoUzytkownika();
-    int pobierzIdNowegoUzytkownika();
-    bool czyIstniejeLogin(string login);
-    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
-    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
-    string konwerjsaIntNaString(int liczba);
-    bool czyPlikJestPusty(fstream &plikTekstowy);
-
+    UzytkownikMenedzer uzytkownikMenedzer;
 
 public:
-    KsiazkaAdresowa();
+
     void rejestracjaUzytkownika();
     void wypiszWszystkichUzytkownikow();
 
 };
+
+#endif
